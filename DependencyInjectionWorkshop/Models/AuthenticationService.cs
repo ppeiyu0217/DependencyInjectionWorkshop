736cb07123d4b4525c665c710354ee568d8a175e
黃 PY <ppeiyu0217@gmail.com>
2019-06-29 17:09:29 +0800

    1. PushMessage 包裝在NotificationDecorator 2. 把INotification抽到建構子(先在方法內加底線 新增到Field 原輸入參數的INotification 可以刪除)

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -2,36 +2,36 @@
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class AuthenticationService
+    public class AuthenticationService 
     {
         private readonly IProfile _profile;
         private readonly IFailedCounter _failedCounter;
-        private readonly IHash _sha256Adapter;
+        private readonly IHash _hash;
         private readonly IOtpService _otpService;
         private readonly ILogger _logger;
-        private readonly INotification _slackAdapter;
+        private readonly INotification _notication;
 
-        public AuthenticationService(ILogger logger, IProfile profile, INotification slackAdapter,
-            IHash sha256Adapter,
+        public AuthenticationService(ILogger logger, IProfile profile, INotification notication,
+            IHash hash,
             IFailedCounter failedCounter,
             IOtpService otpService)
         {
             _profile = profile;
             _failedCounter = failedCounter;
-            _sha256Adapter = sha256Adapter;
+            _hash = hash;
             _otpService = otpService;
             _logger = logger;
-            _slackAdapter = slackAdapter;
+            _notication = notication;
         }
 
         public AuthenticationService()
         {
             _profile = new Profile();
             _failedCounter = new FailedCounter();
-            _sha256Adapter = new Sha256Adapter();
+            _hash = new Sha256Adapter();
             _otpService = new OtpService();
             _logger = new NLogAdapter();
-            _slackAdapter = new SlackAdapter();
+            _notication = new SlackAdapter();
         }
 
         public bool Verify(string accountId, string password, string otp)
@@ -46,7 +46,7 @@ namespace DependencyInjectionWorkshop.Models
             //取得密碼
             var currentPassword = _profile.GetPassword(accountId);
 
-            var hashPassword = _sha256Adapter.Compute(password);
+            var hashPassword = _hash.Compute(password);
 
             //取得Otp
             var currentOtp = _otpService.GetCurrentOtp(accountId);
@@ -67,7 +67,7 @@ namespace DependencyInjectionWorkshop.Models
                 _logger.Info($"accountId:{accountId} failed times:{failedCount}");
 
                 //推播
-                _slackAdapter.PushMessage(accountId);
+                //NotificationDecorator.NotificationWhenInvalid(accountId, _notication);
 
                 return false;
             }
---
+++ DependencyInjectionWorkshop/Models/NotificationDecorator.cs
@@ -0,0 +1,18 @@
+﻿namespace DependencyInjectionWorkshop.Models
+{
+    public class NotificationDecorator
+    {
+        private readonly INotification _notification;
+
+        public NotificationDecorator(INotification notification)
+        {
+            _notification = notification;
+        }
+
+        private void NotificationWhenInvalid(string accountId)
+        {
+            _notification.PushMessage(accountId);
+        }
+        
+    }
+}
