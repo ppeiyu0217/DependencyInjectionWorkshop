5755bd9687da564ceff8037393ff3d27973b0254
黃 PY <ppeiyu0217@gmail.com>
2019-06-30 11:42:36 +0800

    Use Verify

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DependencyInjectionWorkshop.Models;
 
 namespace MyConsole
 {
@@ -10,6 +11,12 @@ namespace MyConsole
     {
         static void Main(string[] args)
         {
+            var authenticationService = new AuthenticationService(new Profile(), new Sha256Adapter(), new OtpService());
+            var notificationDecorator = new NotificationDecorator(authenticationService, new SlackAdapter());
+            var failedCounterDecorator = new FailedCounterDecorator(notificationDecorator, new FailedCounter());
+            var logFailedCount = new LogFailedCountDecorator(failedCounterDecorator, new FailedCounter(), new NLogAdapter());
+            var isValid = logFailedCount.Verify("joey", "9487", "9527");
+            Console.Write(isValid);
         }
     }
 }
