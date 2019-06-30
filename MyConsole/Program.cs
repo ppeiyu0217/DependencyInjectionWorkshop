20415259c919711588dcee0c51a768ce767e8fdd
黃 PY <ppeiyu0217@gmail.com>
2019-06-30 13:54:43 +0800

    用containerBuilder 把要配對的物件跟interface註冊起來，main只要Resolve即可

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
 using DependencyInjectionWorkshop.Models;
 
 namespace MyConsole
 {
     class Program
     {
+        private static IContainer _container;
         //static void Main(string[] args)
         //{
         //    var authenticationService = new AuthenticationService(new Profile(), new Sha256Adapter(), new OtpService());
@@ -19,34 +21,89 @@ namespace MyConsole
         //    Console.Write(isValid);
         //}
 
+        //static void Main(string[] args)
+        //{
+        //    IProfile profile = new FakeProfile();
+        //    //IProfile profile = new ProfileRepo();
+        //    IHash hash = new FakeHash();
+        //    //IHash hash = new Sha256Adapter();
+        //    IOtpService otpService = new FakeOtp();
+        //    //IOtp otpService = new OtpService();
+        //    IFailedCounter failedCounter = new FakeFailedCounter();
+        //    //IFailedCounter failedCounter = new FailedCounter();
+        //    ILogger logger = new ConsoleAdapter();
+        //    //ILogger logger = new NLogAdapter();
+        //    INotification notification = new FakeSlack();
+        //    //INotification notification = new SlackAdapter();
+
+        //    var authenticationService =
+        //        new AuthenticationService(profile, hash, otpService);
+
+        //    var notificationDecorator = new NotificationDecorator(authenticationService, notification);
+        //    var failedCounterDecorator = new FailedCounterDecorator(notificationDecorator, failedCounter);
+        //    var logDecorator = new LogFailedCountDecorator(failedCounterDecorator, failedCounter, logger);
+
+        //    var finalAuthentication = logDecorator; 
+
+        //    var isValid = finalAuthentication.Verify("joey", "pw", "1234563");
+
+        //    Console.WriteLine(isValid);
+        //    Console.ReadLine();
+        //}
+
+
         static void Main(string[] args)
         {
-            IProfile profile = new FakeProfile();
-            //IProfile profile = new ProfileRepo();
-            IHash hash = new FakeHash();
-            //IHash hash = new Sha256Adapter();
-            IOtpService otpService = new FakeOtp();
-            //IOtp otpService = new OtpService();
-            IFailedCounter failedCounter = new FakeFailedCounter();
-            //IFailedCounter failedCounter = new FailedCounter();
-            ILogger logger = new ConsoleAdapter();
-            //ILogger logger = new NLogAdapter();
-            INotification notification = new FakeSlack();
-            //INotification notification = new SlackAdapter();
+            //IProfile profile = new FakeProfile();
+            ////IProfile profile = new ProfileRepo();
+            //IHash hash = new FakeHash();
+            ////IHash hash = new Sha256Adapter();
+            //IOtpService otpService = new FakeOtp();
+            ////IOtp otpService = new OtpService();
+            //IFailedCounter failedCounter = new FakeFailedCounter();
+            ////IFailedCounter failedCounter = new FailedCounter();
+            //ILogger logger = new ConsoleAdapter();
+            ////ILogger logger = new NLogAdapter();
+            //INotification notification = new FakeSlack();
+            ////INotification notification = new SlackAdapter();
 
-            var authenticationService =
-                new AuthenticationService(profile, hash, otpService);
+            //var authenticationService =
+            //    new AuthenticationService(profile, hash, otpService);
+            RegisterContainer();
+            var authenticationService = _container.Resolve<IAuthentication>();
+            //var notificationDecorator = new NotificationDecorator(authenticationService, notification);
+            //var failedCounterDecorator = new FailedCounterDecorator(notificationDecorator, failedCounter);
+            //var logDecorator = new LogFailedCountDecorator(failedCounterDecorator, failedCounter, logger);
 
-            var notificationDecorator = new NotificationDecorator(authenticationService, notification);
-            var failedCounterDecorator = new FailedCounterDecorator(notificationDecorator, failedCounter);
-            var logDecorator = new LogFailedCountDecorator(failedCounterDecorator, failedCounter, logger);
-
-            var finalAuthentication = logDecorator; 
+            var finalAuthentication = authenticationService;
 
             var isValid = finalAuthentication.Verify("joey", "pw", "1234563");
 
             Console.WriteLine(isValid);
             Console.ReadLine();
+        }
+
+        private static void RegisterContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterType<FakeProfile>()
+                .As<IProfile>();
+            containerBuilder.RegisterType<FakeHash>()
+                .As<IHash>();
+            containerBuilder.RegisterType<FakeOtp>()
+                .As<IOtpService>();
+            containerBuilder.RegisterType<FakeSlack>()
+                .As<INotification>();
+            containerBuilder.RegisterType<FakeFailedCounter>()
+                .As<IFailedCounter>();
+            containerBuilder.RegisterType<ConsoleAdapter>()
+                .As<ILogger>();
+
+            containerBuilder.RegisterType<AuthenticationService>()
+                .As<IAuthentication>();
+
+            _container = containerBuilder.Build();
         }
     }
 
@@ -117,6 +174,4 @@ namespace MyConsole
             return "my hashed password";
         }
     }
-
-
 }
